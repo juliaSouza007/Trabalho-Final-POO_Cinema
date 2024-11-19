e478105a73ee7329fc828ef6961f78b5fd65ecc6
Ana Julia <anajufg127@gmail.com>
2024-11-19 15:15:51 -0300

    Delete Cinema directory

--- Cinema/src/main/java/org/teiacoltec/poo/ClassesDAO/CinemaNaoEncontradaException.java
+++
@@ -1,9 +0,0 @@
-package org.teiacoltec.poo.ClassesDAO;
-
-public class CinemaNaoEncontradaException extends Exception {
-
-    public CinemaNaoEncontradaException(int id) {
-        super("Cinema de id " + id + " nao encontrado.");
-    }
-
-}
--- Cinema/src/main/java/org/teiacoltec/poo/ClassesDAO/IdExistenteException.java
+++
@@ -1,8 +0,0 @@
-package org.teiacoltec.poo.ClassesDAO;
-
-public class IdExistenteException extends  Exception {
-    public IdExistenteException(int id) {
-        super("Cinema existente.");
-    }
-
-}
