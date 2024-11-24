20133adb76889ba479a41b2fab44dbfab919f760
Heitor de Sousa Freitas <heitorsf2007@gmail.com>
2024-11-24 11:47:53 -0300

    Create Sala.java

---
+++ Cinema/src/main/java/org/teiacoltec/poo/Classes/Sala.java
@@ -0,0 +1,58 @@
+package org.teiacoltec.poo.Classes;
+
+import Connections.Conexao;
+import org.teiacoltec.poo.ClassesDAO.SalaDAO;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Sala {
+
+    private String nome;
+    private int capacidade;
+    private ArrayList<Sessao> sessao;
+
+
+    public Sala(String nome, int capacidade) {
+
+        this.nome = nome;
+        this.capacidade = capacidade;
+        this.sessao = new ArrayList<>();
+    }
+
+
+    public void criarSessao() {
+       
+    }
+
+    public void listarSessoes() {
+        if (sessao.isEmpty()) {
+            System.out.println("Não há sessões cadastradas.");
+            return;
+        }
+        for (Sessao s : sessao) {
+            System.out.println(s.toString());
+        }
+    }
+
+
+    public String getNome() {
+
+        return nome;
+    }
+
+    public void setNome(String nome) {
+
+        this.nome = nome;
+    }
+
+    public int getCapacidade() {
+
+        return capacidade;
+    }
+
+    public void setCapacidade(int capacidade) {
+
+        this.capacidade = capacidade;
+    }
+
+}
