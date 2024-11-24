811cd3d31514c71e0dbc7da87cb33296802e8718
Ana Julia <anajufg127@gmail.com>
2024-11-24 20:57:43 -0300

    Update Sala.java

--- Cinema/src/main/java/org/teiacoltec/poo/Classes/Sala.java
+++ Cinema/src/main/java/org/teiacoltec/poo/Classes/Sala.java
@@ -2,9 +2,15 @@ package org.teiacoltec.poo.Classes;
 
 import org.teiacoltec.poo.conexao.Conexao;
 import org.teiacoltec.poo.ClassesDAO.SalaDAO;
+import org.teiacoltec.poo.conexao.FalhaConexaoException;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 
 public class Sala {
 
@@ -12,6 +18,7 @@ public class Sala {
     private String nome;
     private int capacidade;
     private ArrayList<Sessao> sessao;
+    private HashMap<Integer, Boolean> assentosDisponiveis;
 
 
     public Sala(int id, String nome, int capacidade) {
@@ -19,6 +26,10 @@ public class Sala {
         this.nome = nome;
         this.capacidade = capacidade;
         this.sessao = new ArrayList<>();
+        this.assentosDisponiveis = new HashMap<>();
+        for (int i = 1; i <= capacidade; i++) {
+            this.assentosDisponiveis.put(i, true); // Todos os assentos começam como disponíveis
+        }
     }
 
 
@@ -40,7 +51,7 @@ public class Sala {
         } catch (SQLException | FalhaConexaoException e) {
             System.err.println("Erro ao criar sessão: " + e.getMessage());
         }
-       
+
     }
 
     public void listarSessoes() {
@@ -52,6 +63,38 @@ public class Sala {
             System.out.println(s.toString());
         }
     }
+
+    public boolean verificarCapacidade() {
+        int assentosOcupados = 0;
+        for (boolean ocupado : assentosDisponiveis.values()) {
+            if (!ocupado) {
+                assentosOcupados++;
+            }
+        }
+
+        if (assentosOcupados < capacidade) {
+            // retorna true caso a capacidade nao tenho sido atingida
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean verificarAssentoDisponivel(int numeroAssento) {
+        return assentosDisponiveis.getOrDefault(numeroAssento, false);
+    }
+
+    public void exibirMapaSala() {
+        System.out.println("Mapa da Sala " + nome + ":");
+        for (int i = 0; i < capacidade; i++) {
+            System.out.print((assentosDisponiveis.get(i) ? "[Livre] " : "[Ocupado] "));
+            if (i % 10 == 0) {
+                System.out.println();
+            }
+        }
+        System.out.println();
+    }
+
 
 
     public String getNome() {
