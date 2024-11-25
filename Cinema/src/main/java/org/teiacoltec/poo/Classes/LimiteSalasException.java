58b6f40cd933845a152fb3c4f96bb640f7f999f9
Ana Julia <anajufg127@gmail.com>
2024-11-24 21:00:49 -0300

    Create LimiteSalasException.java

---
+++ Cinema/src/main/java/org/teiacoltec/poo/Classes/LimiteSalasException.java
@@ -0,0 +1,9 @@
+package org.teiacoltec.poo.Classes;
+
+public class LimiteSalasException extends Exception {
+
+    public LimiteSalasException() {
+        super("Limite de salas alcançado.");
+    }
+
+}
