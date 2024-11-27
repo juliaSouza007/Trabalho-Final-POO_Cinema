1abdf4b082a1d20dfd1c46b3c5831c92190182ac
Ana Julia <anajufg127@gmail.com>
2024-11-26 21:26:59 -0300

    Delete Cinema directory

--- Cinema/src/main/java/org/teiacoltec/poo/Classes/LimiteSalasException.java
+++
@@ -1,9 +0,0 @@
-package org.teiacoltec.poo.Classes;
-
-public class LimiteSalasException extends Exception {
-
-    public LimiteSalasException() {
-        super("Limite de salas alcançado.");
-    }
-
-}
